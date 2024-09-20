5c925b5e10ae75eeb02a22b8f2ae6b50515c8e98
Lucas <lucas.ramos46@fatec.s.gov.br>
2024-09-20 20:40:48 -0300

    feat: aula sobre quicksort

---
+++ aula_0920/ordenacao2.c
@@ -0,0 +1,58 @@
+#include "vetor2.h"
+int main(){
+    int n, *v;
+    srand(time(0));
+    printf("\nDigite o tamanho do vetor, 0 encerra: \n");
+    scanf ("%d", &n);
+    while (n > 0)
+    {
+
+        v =(int *) malloc (n * sizeof(int));
+        
+        //BUBBLESORT
+        preenche_vetor(v, n);
+       // exibe_vetor(v, n, "vetor gerado: ");
+        long inicio = time(0);
+        bubblesort(v, n);
+        long fim = time(0);
+        printf("\nn= %d, tempo bubbleSort = %d", n, fim - inicio);
+       // exibe_vetor(v, n, "vetor ordenado: ");
+
+
+        //INSERTIONSORT
+        preenche_vetor(v, n);
+        //exibe_vetor(v, n, "2o vetor gerado");
+        inicio = time(0);
+        insertionsort(v, n);
+        fim = time(0);
+         printf("\nn= %d, tempo insertionSort = %d", n, fim - inicio);
+        exibe_vetor(v, n, "vetor ordenado insertionSort: ");
+       
+
+
+        //SELECTIONSORT
+        preenche_vetor(v, n);
+        exibe_vetor(v, n, "3o vetor gerado");
+        inicio = time(0);
+        selectionsort(v,n);
+        fim = time(0);
+        printf("\nn= %d, tempo selectionSort = %d", n, fim - inicio);
+        exibe_vetor(v, n, "vetor ordenado selectionSort: ");
+
+
+        //QUICKSORT
+        preenche_vetor(v, n);
+        exibe_vetor(v, n, "4o vetor gerado");
+        inicio = time(0);
+        quicksort(v, 0, n-1);
+        
+        fim = time(0);
+        printf("\nn= %d, tempo do quick = %d", n, fim - inicio);
+        exibe_vetor(v, n, "vetor ordenado quickSort: ");
+
+        free(v);
+        printf("\n Digite o tamanho do vetor para novo teste, 0 encerra: \n");
+        scanf ("%d", &n);     
+    }
+    return 0;
+}
---
+++ aula_0920/vetor2.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void preenche_vetor(int *, int );
+void exibe_vetor (int *, int, char*);
+void soma_vetores(int *, int *, int *, int);
+void bubblesort(int*, int);
+void insertionsort (int*, int);
+void selectionsort (int*, int);
+void quicksort(int *, int, int);
